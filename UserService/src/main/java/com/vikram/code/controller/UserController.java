0e6cbee7f4133471d53caad405f1f36fc193284a
Vikram <vikky5vikram@gmail.com>
2024-08-19 22:02:07 +0530

    Created a Rest API for Find a User By Id in UserController

--- UserService/src/main/java/com/vikram/code/controller/UserController.java
+++ UserService/src/main/java/com/vikram/code/controller/UserController.java
@@ -5,10 +5,7 @@ import com.vikram.code.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -21,11 +18,16 @@ public class UserController {
 
     @PostMapping("/add")
     public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto){
-
         UserDto savedUserDto = userService.createUser(userDto);
-
         return new ResponseEntity<>(savedUserDto, HttpStatus.CREATED);
     }
+
+    @GetMapping("/getUser/{userId}")
+    public ResponseEntity<UserDto> findUserById(@PathVariable String userId){
+        UserDto userDto = userService.getUserById(userId);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
 
 
 }
